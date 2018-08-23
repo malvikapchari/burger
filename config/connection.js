d8151e2f91d2e5635abc116cb36f17db8f8a1bcc
Malvika Patrachari <malvikapatrachari@Malvikas-MacBook-Pro-2.local>
2018-08-23 10:18:22 -0400

    fixed jaws

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,7 @@
 var mysql = require("mysql");
-
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
 var connection = mysql.createConnection({
   host: "localhost",
   port: 8889,
@@ -7,7 +9,7 @@ var connection = mysql.createConnection({
   password: "root",
   database: "burgers_db"
 });
-
+}
 
 connection.connect(function(err) {
   if (err) {
